e23d6c7e16f628091b8683bdc3cc9b0d13b50466
TMandzu <tornikeman@gmail.com>
2016-05-09 06:28:15 +0400

    Mockito add to pom.xml, mockito test in FibonaciTest

--- src/test/java/FibonaciTest.java
+++ src/test/java/FibonaciTest.java
@@ -1,10 +1,20 @@
 /**
  * Created by Tornike on 09.05.2016.
  */
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class FibonaciTest {
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void test_get_method(){
         Fibonaci f = new Fibonaci();
@@ -12,5 +22,11 @@ public class FibonaciTest {
         assertEquals(f.get(3), 3);
         assertEquals(f.get(4), 5);
         assertEquals(f.get(5), 8);
+
+        f = Mockito.mock(Fibonaci.class);
+        when(f.get(4)).thenReturn(4);
+        when(f.get(5)).thenReturn(5);
+        assertEquals(f.get(4), 4);
+        assertEquals(f.get(5), 5);
     }
 }
